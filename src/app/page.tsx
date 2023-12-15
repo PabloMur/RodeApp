b15bce72da7bdd70d32982544c5c92168dfd5142
PabloMur <polillomurillo@gmail.com>
2023-12-15 17:17:27 -0300

    text center en el banner de bienvenida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
       className={`${css.mainContainer} flex flex-col justify-center items-center min-h-[88vh] w-full bg-white flex-grow relative`}
     >
       <h1 className="text-2xl font-bold z-10">Bienvenido a RodeApp</h1>
-      <h3 className="z-20">Una app hecha por moteros, para moteros</h3>
+      <h3 className="z-20 text-center">
+        Organiza todo para tu viaje en un solo lugar!
+      </h3>
       <div className="h-full w-full absolute top-0 bg-black/50"></div>
     </div>
   );
