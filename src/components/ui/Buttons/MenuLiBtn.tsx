97a3abbbe9c1f9a07e7557b044e2afd2acfc3147
PabloMur <polillomurillo@gmail.com>
2023-12-15 17:42:28 -0300

    estilos en menu li

--- src/components/ui/Buttons/MenuLiBtn.tsx
+++ src/components/ui/Buttons/MenuLiBtn.tsx
@@ -1,3 +1,7 @@
 export default function ManuLiBtn({ content }: any) {
-  return <button>{content}</button>;
+  return (
+    <button className="text-black rounded-full border border-black p-3 m-1 w-[90%]">
+      {content}
+    </button>
+  );
 }
