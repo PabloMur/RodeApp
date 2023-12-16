16743c218fbfebf1c00d3abe4661d22b6ce4a8a4
PabloMur <polillomurillo@gmail.com>
2023-12-16 04:41:55 -0300

    add some benefits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import BenefitCard from "@/components/cards/BenefitsCard";
 import TestimonialCard from "@/components/cards/TestimonialCard";
 import CtaBtn from "@/components/ui/Buttons/CtaBtn";
 import css from "@/styles/home.module.css";
@@ -16,7 +17,7 @@ export default function Home() {
       </div>
       <section className="min-h-[100vh] w-full flex flex-col justify-center items-center p-5">
         <h2 className="text-bold text-2xl">Que es RodeApp?</h2>
-        <p className="text-center">
+        <p className="text-justify">
           En RodeApp, nos apasiona convertir cada viaje en moto en una
           experiencia inolvidable. Somos motoviajeros, exploradores de
           carreteras y amantes de la libertad sobre dos ruedas. Nuestra misión
@@ -31,7 +32,53 @@ export default function Home() {
           viajes en moto con RodeApp!
         </p>
       </section>
-      <section className="h-[100vh] w-full bg-green-400">Beneficios</section>
+      <section className="min-h-[100vh] w-full bg-green-400 p-2 gap-2 flex flex-col">
+        <BenefitCard
+          title="Planificación Intuitiva"
+          description="Trazar rutas personalizadas y descubrir destinos emocionantes nunca fue tan fácil."
+          icon="https://images.pexels.com/photos/1252500/pexels-photo-1252500.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
+        <BenefitCard
+          title="Listas de Verificación Personalizadas"
+          description="Crea y gestiona listas de ítems esenciales para tu viaje, desde equipo hasta documentos."
+          icon="https://images.pexels.com/photos/416322/pexels-photo-416322.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
+        <BenefitCard
+          title="Meteorología en Tiempo Real"
+          description="Mantente informado sobre las condiciones climáticas actuales y futuras en tu ruta."
+          icon="https://images.pexels.com/photos/17514177/pexels-photo-17514177/free-photo-of-mujer-joven-que-usa-la-nueva-aplicacion-de-subprocesos-meta-en-el-telefono-inteligente.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
+        <BenefitCard
+          title="Registro de Gastos y Alojamiento"
+          description="Controla tu presupuesto con estimaciones de gastos y opciones de alojamiento a lo largo de tu viaje."
+          icon="/images/expenses-icon.png"
+        />
+        <BenefitCard
+          title="Comunidad Motera"
+          description="Conéctate con otros moteros, comparte experiencias y descubre recomendaciones de lugares de interés."
+          icon="/images/community-icon.png"
+        />
+        <BenefitCard
+          title="Calculadora de Combustible"
+          description="Obtén estimaciones precisas de consumo de combustible según las características de tu moto y tu ruta."
+          icon="/images/fuel-icon.png"
+        />
+        <BenefitCard
+          title="Navegación GPS"
+          description="Utiliza la función de seguimiento en tiempo real para navegar como un profesional y nunca perderte."
+          icon="/images/gps-icon.png"
+        />
+        <BenefitCard
+          title="Sistema de Valoración y Reseñas"
+          description="Descubre y comparte recomendaciones sobre restaurantes, posadas y lugares de interés en tu ruta."
+          icon="/images/reviews-icon.png"
+        />
+        <BenefitCard
+          title="Funcionalidades Escalables"
+          description="RodeApp crece contigo, con futuras funciones como sistema de baliza y suscripciones para descuentos exclusivos."
+          icon="/images/scalable-icon.png"
+        />
+      </section>
       <section className="min-h-[100vh] w-full">
         Testimonios
         <TestimonialCard
---
+++ src/components/cards/BenefitsCard.tsx
@@ -0,0 +1,13 @@
+const BenefitCard = ({ title, description, icon }: any) => {
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-md">
+      <div className="text-center mb-4">
+        <img src={icon} alt={title} className="w-16 h-16 mx-auto mb-2" />
+        <h3 className="text-lg font-semibold">{title}</h3>
+      </div>
+      <p className="text-gray-700">{description}</p>
+    </div>
+  );
+};
+
+export default BenefitCard;
