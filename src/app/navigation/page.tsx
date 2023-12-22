310ad4bcf8360c1a76f14060f5ff3d01fb08fc23
PabloMur <polillomurillo@gmail.com>
2023-12-22 01:41:50 -0300

    asd

--- src/app/navigation/page.tsx
+++ src/app/navigation/page.tsx
@@ -1,9 +1,3 @@
-import MyComponent from "@/components/NavigationComp";
 export default function Navigation() {
-  return (
-    <div>
-      Navigation page
-      <MyComponent></MyComponent>
-    </div>
-  );
+  return <div>Navigation page</div>;
 }
