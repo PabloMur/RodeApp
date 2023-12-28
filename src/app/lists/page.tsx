110641a8c97547db259325bfbdfb548e76f0cb58
PabloMur <polillomurillo@gmail.com>
2023-12-27 22:37:23 -0300

    asd

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,9 +1,20 @@
+"use client"
 import ListForm from "@/components/forms/CreateList";
+import { useGoTo } from "@/hooks";
 
 export default function Lists() {
+  const goto = useGoTo();
   return (
     <div className="p-4">
       <ListForm></ListForm>
+      <button
+        className="bg-orange-500 text-black p-4 rounded-xl mt-4 text-2xl w-full"
+        onClick={() => {
+          goto("/my-lists");
+        }}
+      >
+        Mis listas
+      </button>
     </div>
   );
 }
---
+++ src/app/my-lists/page.tsx
@@ -0,0 +1,3 @@
+export default function MyListPage() {
+  return <div> mis listas </div>;
+}
