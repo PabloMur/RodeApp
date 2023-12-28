00b102ecd3231faed8c3b99e7e0364c5a546f44d
PabloMur <polillomurillo@gmail.com>
2023-12-27 22:51:13 -0300

    history icon

--- src/components/RecentList.tsx
+++ src/components/RecentList.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+import historyIcon from "../../public/history.svg";
+
 export default function RecentList() {
   return (
     <div className="border-2 border-orange-500 rounded-2xl text-orange-500 p-2 w-full">
       <p className="m-2">Lista reciente:</p>
-      <div className="bg-orange-500 w-full p-4 text-black rounded-xl">
-        Titulo de la ultima lista
+      <div className="bg-orange-500 w-full p-4 text-black rounded-xl flex justify-between items-center">
+        <p className="text-xl">Titulo de la ultima lista</p>
+        <Image
+          src={historyIcon}
+          alt="history icon"
+          width={25}
+          height={25}
+        ></Image>
       </div>
     </div>
   );
