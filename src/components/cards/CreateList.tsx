02ae4901df69fe88b9244b988fb6f3bfb7e7ebba
PabloMur <polillomurillo@gmail.com>
2024-01-03 11:22:37 -0300

    plus icon on create list

--- src/components/cards/CreateList.tsx
+++ src/components/cards/CreateList.tsx
@@ -1,15 +1,18 @@
 "use client";
 import { useGoTo } from "@/hooks";
+import Image from "next/image";
+import plus from "../../../public/plus.svg";
 export default function CreateListCard() {
   const goto = useGoTo();
   return (
     <div
-      className=" rounded-xl bg-orange-500 w-1/2 p-4 flex justify-center items-center h-32 text-black text-xl box-border cursor-pointer"
+      className=" rounded-xl bg-orange-500 w-1/2 p-4 flex justify-center items-center h-32 text-black text-xl box-border cursor-pointer gap-2"
       onClick={() => {
         goto("/lists");
       }}
     >
-      Crear Lista
+      <p className="text-xl">Crear lista</p>
+      <Image src={plus} alt={"icono de plus"} width={25} height={25}></Image>
     </div>
   );
 }
