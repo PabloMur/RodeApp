15d5dc7ce632a0059dc72e2027eacf5050f63d85
PabloMur <pablomurillo.sp@gmail.com>
2024-01-10 23:58:55 -0300

    nuevo color de texto en landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
       <div
         className={`${css.mainContainer} flex flex-col justify-center items-center min-h-[88vh] w-full bg-white flex-grow relative`}
       >
-        <h1 className="text-2xl font-bold z-10">Bienvenido a RodeApp</h1>
-        <h3 className="z-20 text-center">
+        <h1 className="text-2xl font-bold z-10 text-white">Bienvenido a RodeApp</h1>
+        <h3 className="z-20 text-center text-white">
           Organiza todo para tu viaje en un solo lugar!
         </h3>
         <CtaBtn></CtaBtn>
