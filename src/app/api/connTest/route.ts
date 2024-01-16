f5441eb22b31fc34c0afc43325901851a47839cc
PabloMur <polillomurillo@gmail.com>
2024-01-16 14:16:31 -0300

    test

--- src/app/api/connTest/route.ts
+++ src/app/api/connTest/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function GET(req: NextRequest) {
   console.log(firestore);
+  console.log(process.env.FIRESTORE_API_KEY);
   return NextResponse.json({
     hola: "hola",
   });
