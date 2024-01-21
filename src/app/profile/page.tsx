0cec7d87381eddcf563b1ce3e2419ce2bfb0443b
PabloMur <polillomurillo@gmail.com>
2024-01-21 13:40:40 -0300

    a

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,7 +2,7 @@ import ProfileDetails from "@/components/ProfileGadget";
 
 export default function Profile() {
   return (
-    <div className="p-4">
+    <div className="p-4 min-h-[88vh] flex justify-center items-center">
       <ProfileDetails></ProfileDetails>
     </div>
   );
